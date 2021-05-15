d9cddb19fed10addd1bf6869b868ee237bf7f133
NISHIDO-LAPTOP\admin <quan5555qt@gmail.com>
2021-05-15 21:14:48 +0700

    Update : auto change slideshow every 3 secs

--- app/static/bodyWeb/script/adsScript.js
+++ app/static/bodyWeb/script/adsScript.js
@@ -23,6 +23,9 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
+
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+setInterval(function(){ plusSlides(1); }, 3000);
